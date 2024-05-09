64a80427663985e59f8c560945de11665c61f04c
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-09 15:30:10 -0300

    feat: popular pictures

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -7,6 +7,7 @@ import Galeria from "./componentes/Galeria";
 import bannerBackground from './assets/banner.png'
 import fotos from './fotos.json';
 import { useState } from "react";
+import ModalZoom from "./componentes/ModalZoom";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -32,6 +33,7 @@ const ConteudoGaleria = styled.section`
 
 const App = () => {
   const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
+  const [fotoSelecionada, setFotoSelecionada] = useState(null)
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -41,10 +43,14 @@ const App = () => {
           <BarraLateral />
           <ConteudoGaleria>
             <Banner texto="A galeria mais completa de fotos do espaço!" backgroundImage={bannerBackground} />
-            <Galeria fotos={fotosDaGaleria} />
+            <Galeria 
+              aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+              fotos={fotosDaGaleria}
+            />
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
+      <ModalZoom foto={fotoSelecionada} />
     </FundoGradiente>
   )
 }
